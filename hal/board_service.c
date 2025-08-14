5dd456df1689dff853ba84a35cf37fae4321d854
Yuuki <yuuzena@gmail.com>
2025-08-14 13:50:52 +0200

    Reduce Tokens

--- hal/board_service.c
+++ hal/board_service.c
@@ -1,37 +1,3 @@
-/*******************************************************************************
-* Copyright (c) 2020 released Microchip Technology Inc.  All rights reserved.
-*
-* SOFTWARE LICENSE AGREEMENT:
-* 
-* Microchip Technology Incorporated ("Microchip") retains all ownership and
-* intellectual property rights in the code accompanying this message and in all
-* derivatives hereto.  You may use this code, and any derivatives created by
-* any person or entity by or on your behalf, exclusively with Microchip's
-* proprietary products.  Your acceptance and/or use of this code constitutes
-* agreement to the terms and conditions of this notice.
-*
-* CODE ACCOMPANYING THIS MESSAGE IS SUPPLIED BY MICROCHIP "AS IS".  NO
-* WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
-* TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-* PARTICULAR PURPOSE APPLY TO THIS CODE, ITS INTERACTION WITH MICROCHIP'S
-* PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-*
-* YOU ACKNOWLEDGE AND AGREE THAT, IN NO EVENT, SHALL MICROCHIP BE LIABLE,
-* WHETHER IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF
-* STATUTORY DUTY),STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE,
-* FOR ANY INDIRECT, SPECIAL,PUNITIVE, EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL
-* LOSS, DAMAGE, FOR COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE CODE,
-* HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR
-* THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT ALLOWABLE BY LAW,
-* MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS CODE,
-* SHALL NOT EXCEED THE PRICE YOU PAID DIRECTLY TO MICROCHIP SPECIFICALLY TO
-* HAVE THIS CODE DEVELOPED.
-*
-* You agree that you are solely responsible for testing the code and
-* determining its suitability.  Microchip has no obligation to modify, test,
-* certify, or support the code.
-*
-*******************************************************************************/
 #include <stdint.h>
 #include <stdbool.h>
 #include "board_service.h"
@@ -98,11 +64,8 @@ void BoardService(void)
 {
     if (boardServiceISRCounter ==  BOARD_SERVICE_TICK_COUNT)
     {
-        /* Button scanning loop for Button 1 to start Motor A */
         ButtonScan(&buttonStartStop,BUTTON_START_STOP);
 
-        /* Button scanning loop for SW2 to enter into filed
-            weakening mode */
         ButtonScan(&buttonSpeedHalfDouble,BUTTON_SPEED_HALF_DOUBLE);
 
         boardServiceISRCounter = 0;
@@ -147,36 +110,13 @@ void ButtonGroupInitialize(void)
     buttonSpeedHalfDouble.debounceCount = 0;
     buttonSpeedHalfDouble.state = false;	  
 }
-/* Function:
-    InitPeripherals()
 
-  Summary:
-    Routine initializes controller peripherals
-
-  Description:
-    Routine to initialize Peripherals used for Inverter Control
-
-  Precondition:
-    None.
-
-  Parameters:
-    None
-
-  Returns:
-    None.
-
-  Remarks:
-    None.
- */
 void Init_Peripherals(void)
 {                
-	/* Configures the clock settings (MIPS) */
     InitOscillator();
     
-    /* Configures and Maps GPIO Function */
     SetupGPIOPorts();  
     
-    /*Configures the Comparator#1 for PCI fault*/
     uint16_t cmpReference = 0;
     CMP_Initialize();
     CMP1_ModuleEnable(true);
@@ -184,69 +124,41 @@ void Init_Peripherals(void)
     cmpReference = cmpReference + 2048; 
     CMP1_ReferenceSet(cmpReference);
     
-    /* Initialize PWM generators */
     InitPWMGenerators();
     
-    /* Initialize the ADC used for sensing Inverter A parameters */
     InitializeADCs();
 }
 
 void EnablePWMOutputs(void)
 {
-    //PWM controls signals in complementary mode, over-ride disabled
-	// 0 = PWM Generator provides data for the PWM4H pin
     PG4IOCONLbits.OVRENH = 0; 
-    // 0 = PWM Generator provides data for the PWM4L pin
     PG4IOCONLbits.OVRENL = 0; 
-    
-    // 0 = PWM Generator provides data for the PWM2H pin
     PG2IOCONLbits.OVRENH = 0;
-    // 0 = PWM Generator provides data for the PWM2L pin
     PG2IOCONLbits.OVRENL = 0; 
-    
-    // 0 = PWM Generator provides data for the PWM1H pin
     PG1IOCONLbits.OVRENH = 0;  
-    // 0 = PWM Generator provides data for the PWM1L pin
     PG1IOCONLbits.OVRENL = 0;   
 }
 
 void DisablePWMOutputs(void)
 {
-    /** Set Override Data on all PWM outputs */
-    // 0b00 = State for PWM4H,L, if Override is Enabled
     PG4IOCONLbits.OVRDAT = 0;
-    // 0b00 = State for PWM2H,L, if Override is Enabled
     PG2IOCONLbits.OVRDAT = 0; 
-    // 0b00 = State for PWM1H,L, if Override is Enabled
     PG1IOCONLbits.OVRDAT = 0;  
-    
-    // 1 = OVRDAT<1> provides data for output on PWM4H
     PG4IOCONLbits.OVRENH = 1; 
-    // 1 = OVRDAT<0> provides data for output on PWM4L
     PG4IOCONLbits.OVRENL = 1; 
-    
-    // 1 = OVRDAT<1> provides data for output on PWM2H
     PG2IOCONLbits.OVRENH = 1;
-    // 1 = OVRDAT<0> provides data for output on PWM2L
     PG2IOCONLbits.OVRENL = 1; 
-    
-    // 1 = OVRDAT<1> provides data for output on PWM1H
     PG1IOCONLbits.OVRENH = 1;  
-    // 1 = OVRDAT<0> provides data for output on PWM1L
     PG1IOCONLbits.OVRENL = 1;     
 }
 
 void ClearPWMPCIFault(void)
 {
-    /* SWTERM: PCI Software Termination bit
-       A write of ?1? to this location will produce a termination event*/
     PG1FPCILbits.SWTERM = 1;
     PG2FPCILbits.SWTERM = 1;
     PG4FPCILbits.SWTERM = 1;
 }
 
-/*PWM Mode 4 - Center-Aligned Mode for Dual Shunt operation
-  Write to PGxDC registers*/
 void PWMDutyCycleSet(MC_DUTYCYCLEOUT_T *pPwmDutycycle)
 {
     pwmDutyCycleLimitCheck(pPwmDutycycle,(DDEADTIME>>1),(LOOPTIME_TCY - (DDEADTIME>>1)));  
@@ -255,8 +167,6 @@ void PWMDutyCycleSet(MC_DUTYCYCLEOUT_T *pPwmDutycycle)
     PWM_PDC1 = pPwmDutycycle->dutycycle1;
 }
 
-/*PWM Mode 6 - Dual Edge Center-Aligned PWM Mode - Single update per cycle for Single Shunt
-  Write to PGxDC and PGxPHASE */
 void PWMDutyCycleSetDualEdge(MC_DUTYCYCLEOUT_T *pPwmDutycycle1,MC_DUTYCYCLEOUT_T *pPwmDutycycle2)
 {
     pwmDutyCycleLimitCheck(pPwmDutycycle1,(DDEADTIME>>1),(LOOPTIME_TCY - (DDEADTIME>>1)));
@@ -272,7 +182,6 @@ void PWMDutyCycleSetDualEdge(MC_DUTYCYCLEOUT_T *pPwmDutycycle1,MC_DUTYCYCLEOUT_T
     PWM_PDC1 = pPwmDutycycle2->dutycycle1 - (DDEADTIME>>1);
 }
 
-/*Limiting the PWM duty ratio within specified limits */
 void pwmDutyCycleLimitCheck (MC_DUTYCYCLEOUT_T *pPwmDutycycle,uint16_t min,uint16_t max)
 {
     if (pPwmDutycycle->dutycycle1 < min)
--- hal/measure.c
+++ hal/measure.c
@@ -1,75 +1,9 @@
-// <editor-fold defaultstate="collapsed" desc="Description/Instruction ">
-/**
- * @file measure.c
- *
- * @brief This module has functions for signal conditioning of measured
- *        analog feedback signals.
- *
- * Component: MEASURE
- *
- */
-// </editor-fold>
-
-// <editor-fold defaultstate="collapsed" desc="Disclaimer ">
-
-/*******************************************************************************
-* Copyright (c) 2017 released Microchip Technology Inc.  All rights reserved.
-*
-* SOFTWARE LICENSE AGREEMENT:
-*
-* Microchip Technology Incorporated ("Microchip") retains all ownership and
-* intellectual property rights in the code accompanying this message and in all
-* derivatives hereto.  You may use this code, and any derivatives created by
-* any person or entity by or on your behalf, exclusively with Microchip's
-* proprietary products.  Your acceptance and/or use of this code constitutes
-* agreement to the terms and conditions of this notice.
-*
-* CODE ACCOMPANYING THIS MESSAGE IS SUPPLIED BY MICROCHIP "AS IS".  NO
-* WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
-* TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-* PARTICULAR PURPOSE APPLY TO THIS CODE, ITS INTERACTION WITH MICROCHIP'S
-* PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-*
-* YOU ACKNOWLEDGE AND AGREE THAT, IN NO EVENT, SHALL MICROCHIP BE LIABLE,
-* WHETHER IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF
-* STATUTORY DUTY),STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE,
-* FOR ANY INDIRECT, SPECIAL,PUNITIVE, EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL
-* LOSS, DAMAGE, FOR COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE CODE,
-* HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR
-* THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT ALLOWABLE BY LAW,
-* MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO THIS CODE,
-* SHALL NOT EXCEED THE PRICE YOU PAID DIRECTLY TO MICROCHIP SPECIFICALLY TO
-* HAVE THIS CODE DEVELOPED.
-*
-* You agree that you are solely responsible for testing the code and
-* determining its suitability.  Microchip has no obligation to modify, test,
-* certify, or support the code.
-*
-*******************************************************************************/
-// </editor-fold>
-
-// <editor-fold defaultstate="collapsed" desc="HEADER FILES ">
-
 #include <stdint.h>
 #include <stdbool.h>
 
 #include "measure.h"
 #include "adc.h"
 
-// </editor-fold>
-
-/**
-* <B> Function: MCAPP_MeasureCurrentInit(MCAPP_MEASURE_CURRENT_T *)  </B>
-*
-* @brief Function to reset variables used for current offset measurement.
-*        .
-*
-* @param Pointer to the data structure containing measured currents.
-* @return none.
-* @example
-* <CODE> MCAPP_MeasureCurrentInit(&current); </CODE>
-*
-*/
 void MCAPP_MeasureCurrentInit(MCAPP_MEASURE_T *pMotorInputs)
 {
     MCAPP_MEASURE_CURRENT_T *pCurrent;
@@ -83,18 +17,6 @@ void MCAPP_MeasureCurrentInit(MCAPP_MEASURE_T *pMotorInputs)
     pCurrent->status = 0;
 }
 
-/**
-* <B> Function: MCAPP_MeasureCurrentOffset(MCAPP_MEASURE_CURRENT_T *)  </B>
-*
-* @brief Function to compute current offset after measuring specified number of
-*        current samples and averaging them.
-*        .
-* @param Pointer to the data structure containing measured current.
-* @return none.
-* @example
-* <CODE> MCAPP_MeasureCurrentOffset(&current); </CODE>
-*
-*/
 void MCAPP_MeasureCurrentOffset(MCAPP_MEASURE_T *pMotorInputs)
 {
     MCAPP_MEASURE_CURRENT_T *pCurrent;
@@ -120,17 +42,7 @@ void MCAPP_MeasureCurrentOffset(MCAPP_MEASURE_T *pMotorInputs)
         pCurrent->status = 1;
     }
 }
-/**
-* <B> Function: MCAPP_MeasureCurrentCalibrate(MCAPP_MEASURE_CURRENT_T *)  </B>
-*
-* @brief Function to compensate offset from measured current samples.
-*        .
-* @param Pointer to the data structure containing measured current.
-* @return none.
-* @example
-* <CODE> MCAPP_MeasureCurrentCalibrate(&current); </CODE>
-*
-*/
+
 void MCAPP_MeasureCurrentCalibrate(MCAPP_MEASURE_T *pMotorInputs)
 {
     MCAPP_MEASURE_CURRENT_T *pCurrent;
@@ -146,17 +58,6 @@ int16_t MCAPP_MeasureCurrentOffsetStatus (MCAPP_MEASURE_T *pMotorInputs)
     return pMotorInputs->current.status;
 }
 
-/**
-* <B> Function: void MC_MovingAvgFilterInit(MC_MOVING_AVG_T *)              </B>
-*
-* @brief Function to reset moving average filter history .
-*
-* @param none.
-* @return none.
-* @example
-* <CODE> MC_MovingAvgFilterInit(&filterData); </CODE>
-*
-*/
 void MCAPP_MeasureAvgInit(MCAPP_MEASURE_AVG_T *pFilterData,uint16_t scaler)
 {
     pFilterData->scaler = scaler;
@@ -165,17 +66,7 @@ void MCAPP_MeasureAvgInit(MCAPP_MEASURE_AVG_T *pFilterData,uint16_t scaler)
     pFilterData->sum = 0;
     pFilterData->status = 0;
 }
-/**
-* <B> Function: void MC_MovingAvgFilter(MC_MOVING_AVG_T *,int16_t )         </B>
-*
-* @brief Function implementing moving average filter .
-*
-* @param none.
-* @return none.
-* @example
-* <CODE> MC_MovingAvgFilter(&filterData,data );                          </CODE>
-*
-*/
+
 int16_t MCAPP_MeasureAvg(MCAPP_MEASURE_AVG_T *pFilterData)
 {    
     pFilterData->sum += pFilterData->input;
@@ -193,17 +84,7 @@ int16_t MCAPP_MeasureAvg(MCAPP_MEASURE_AVG_T *pFilterData)
     }
     return pFilterData->avg;
 }
-/**
-* <B> Function: void MC_MovingAvgFilterInit(MC_MOVING_AVG_T *)              </B>
-*
-* @brief Function to reset moving average filter history .
-*
-* @param none.
-* @return none.
-* @example
-* <CODE> MC_MovingAvgFilterInit(&filterData); </CODE>
-*
-*/
+
 void MCAPP_MeasureTemperature(MCAPP_MEASURE_T *pData, int16_t input)
 {
     pData->MOSFETTemperature.input = input;
